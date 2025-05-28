cd44ad12c361186bb71860f00145221dcfffee77
NatashaRJ <natasharj07@gmail.com>
2025-05-28 10:11:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appfirestoreCRUD',
+  webDir: 'www'
+};
+
+export default config;
